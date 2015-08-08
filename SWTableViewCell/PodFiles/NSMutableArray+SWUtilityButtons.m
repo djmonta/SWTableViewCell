a927b2311e214c153a11bcd874d4fe7bf6f921df
monta <djmonta@gmail.com>
2015-08-08 17:17:00 +0900

    Carthage Support

--- SWTableViewCell/PodFiles/NSMutableArray+SWUtilityButtons.m
+++ SWTableViewCell/PodFiles/NSMutableArray+SWUtilityButtons.m
@@ -7,6 +7,7 @@
 //
 
 #import "NSMutableArray+SWUtilityButtons.h"
+#import <UIKit/UIKit.h>
 
 @implementation NSMutableArray (SWUtilityButtons)
 
--- SWTableViewCellTests/SWTableViewCellTests.m
+++ SWTableViewCellTests/SWTableViewCellTests.m
@@ -1,75 +1,40 @@
-#import <Specta/Specta.h>
-#import <Expecta/Expecta.h>
-#import <FBSnapshotTestCase/FBSnapshotTestCase.h>
-#import <Expecta+Snapshots/EXPMatchers+FBSnapshotTest.h>
-#import <OCMock/OCMock.h>
-#import "SWTableViewCell.h"
+//
+//  SWTableViewCellTests.m
+//  SWTableViewCellTests
+//
+//  Created by 宮本幸子 on 2015/08/08.
+//  Copyright (c) 2015年 Chris Wendel. All rights reserved.
+//
 
-SpecBegin(SWTableViewCell)
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
 
-__block NSArray *rightButtons;
-__block NSArray *leftButtons;
+@interface SWTableViewCellTests : XCTestCase
 
-before(^{
-    NSMutableArray *rightUtilityButtons = [NSMutableArray new];
-    [rightUtilityButtons sw_addUtilityButtonWithColor:
-     [UIColor colorWithRed:0.78f green:0.78f blue:0.8f alpha:1.0]
-                                                title:@"More"];
-    [rightUtilityButtons sw_addUtilityButtonWithColor:
-     [UIColor colorWithRed:1.0f green:0.231f blue:0.188 alpha:1.0f]
-                                                title:@"Delete"];
+@end
 
-    rightButtons = rightUtilityButtons;
-    
-    
-    NSMutableArray *leftUtilityButtons = [NSMutableArray new];
-    
-    [leftUtilityButtons sw_addUtilityButtonWithColor:
-     [UIColor colorWithRed:0.07 green:0.75f blue:0.16f alpha:1.0]
-                                                icon:[UIImage imageNamed:@"check.png"]];
-    [leftUtilityButtons sw_addUtilityButtonWithColor:
-     [UIColor colorWithRed:1.0f green:1.0f blue:0.35f alpha:1.0]
-                                                icon:[UIImage imageNamed:@"clock.png"]];
-    [leftUtilityButtons sw_addUtilityButtonWithColor:
-     [UIColor colorWithRed:1.0f green:0.231f blue:0.188f alpha:1.0]
-                                                icon:[UIImage imageNamed:@"cross.png"]];
-    [leftUtilityButtons sw_addUtilityButtonWithColor:
-     [UIColor colorWithRed:0.55f green:0.27f blue:0.07f alpha:1.0]
-                                                icon:[UIImage imageNamed:@"list.png"]];
-    
-    leftButtons = leftUtilityButtons;
-    
-});
+@implementation SWTableViewCellTests
 
-describe(@"init", ^{
-    it(@"should init with cell style UITableViewStyleDefault", ^{
-        SWTableViewCell *cell = [[SWTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
-        expect(cell).toNot.beNil;
-    });
-    
-    it(@"should init with cell style UITableViewStyleSubtitle", ^{
-        SWTableViewCell *cell = [[SWTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil];
-        expect(cell).toNot.beNil;
-    });
-});
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
 
-describe(@"buttons", ^{
-    __block SWTableViewCell *cell;
-    
-    before(^{
-        cell = [[SWTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
-        [cell setRightUtilityButtons:rightButtons WithButtonWidth:44.0f];
-        [cell setLeftUtilityButtons:leftButtons WithButtonWidth:44.0f];
-    });
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
 
-    it(@"should have two right buttons", ^{
-        expect(cell.rightUtilityButtons.count).to.equal(2);
-    });
-    
-    it(@"should have four left buttons", ^{
-        expect(cell.leftUtilityButtons.count).to.equal(4);
-    });
-});
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
 
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
 
-SpecEnd
+@end
